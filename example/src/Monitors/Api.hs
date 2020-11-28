be7b7a7f28b4d347ddf4b9681afc5f80325fdf5c
Lucas DiCioccio <lucas@dicioccio.fr>
2020-11-28 11:18:14 +0100

    Show off cookie protection.

--- example/src/Monitors/Api.hs
+++ example/src/Monitors/Api.hs
@@ -4,14 +4,17 @@ module Monitors.Api where
 
 import Servant
 import Monitors.Base
+import Prod.UserAuth.Api (CookieProtect)
 
 type Api = "monitors" :>
   ( AddPing :<|> RemovePing )
 
 type AddPing = "ping"
+  :> CookieProtect
   :> ReqBody '[JSON] PingTarget 
   :> Post '[JSON] Registration
 
 type RemovePing = "ping"
+  :> CookieProtect
   :> ReqBody '[JSON] Registration
   :> Delete '[JSON] DeRegistration
--- example/src/Monitors/Handlers.hs
+++ example/src/Monitors/Handlers.hs
@@ -10,19 +10,20 @@ import Data.IORef (atomicModifyIORef')
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.List as List
 import Prod.Background (kill)
+import Prod.UserAuth (UserAuthInfo, authorized)
 
 handle :: Runtime -> Server Api
 handle rt = addPing rt :<|> removePing rt
 
-addPing :: Runtime -> PingTarget -> Handler Registration
-addPing rt tgt@(PingTarget target) = liftIO $ do
+addPing :: Runtime -> UserAuthInfo -> PingTarget -> Handler Registration
+addPing rt auth tgt@(PingTarget target) = authorized auth $ \id -> liftIO $ do
   bkg <- backgroundPings (counters rt) tgt
   let regname = target
   _ <- atomicModifyIORef' (pings rt) (\xs -> ((regname, bkg) : xs, ()))
   pure $ regname
 
-removePing :: Runtime -> Registration -> Handler DeRegistration
-removePing rt regname = liftIO $ do
+removePing :: Runtime -> UserAuthInfo -> Registration -> Handler DeRegistration
+removePing rt auth regname = authorized auth $ \id -> liftIO $ do
   let kept (r, _) = r /= regname
   bkgs <- atomicModifyIORef' (pings rt) (List.partition kept)
   traverse kill $ fmap snd $ bkgs
