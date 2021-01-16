cbc7bf656c450361de0aaaa92bf07939ca1bb054
Lucas DiCioccio <lucas@dicioccio.fr>
2021-01-16 11:50:50 +0100

    Add combinator to sequentially trace twice a same value.

--- prodapi/src/Prod/Tracer.hs
+++ prodapi/src/Prod/Tracer.hs
@@ -1,6 +1,6 @@
 -- https://www.youtube.com/watch?v=qzOQOmmkKEM&feature=emb_logo
 
-module Prod.Tracer (Tracer(..), Contravariant(..)) where
+module Prod.Tracer (Tracer(..), Contravariant(..), traceBoth) where
 
 import Data.Functor.Contravariant
 
@@ -9,3 +9,7 @@ newtype Tracer m a = Tracer { runTracer :: (a -> m ()) }
 instance Contravariant (Tracer m) where
   contramap f (Tracer g) = Tracer (g . f)
 
+-- | If you are given two tracers and want to pass both.
+-- Composition occurs in sequence.
+traceBoth :: (Applicative m) => Tracer m a -> Tracer m a -> Tracer m a
+traceBoth (Tracer f1) (Tracer f2) = Tracer (\a -> f1 a *> f2 a)
